9b82452cf02c9ab39bd5d57ca46602759e619590
Sushanto.171 <sushantochandrasharkar171@gmail.com>
2025-09-04 14:35:07 +0600

    add refresh-token mechanism

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,5 @@
 import { config } from "@/config";
-import axios, { AxiosError } from "axios";
+import axios, { AxiosError, type AxiosRequestConfig } from "axios";
 export const axiosInstance = axios.create({
   baseURL: config.baseUrl,
   withCredentials: true,
@@ -12,9 +12,55 @@ axiosInstance.interceptors.request.use(
   }
 );
 
+let isRefreshing = false;
+let pendingQueue: {
+  resolve: (value: unknown) => void;
+  reject: (value: unknown) => void;
+}[] = [];
+
+const processQueue = (error: unknown) => {
+  pendingQueue.forEach((promise) => {
+    if (error) {
+      promise.reject(error);
+    } else {
+      promise.resolve(null);
+    }
+  });
+  pendingQueue = [];
+};
+
 axiosInstance.interceptors.response.use(
   (response) => response,
-  (error: AxiosError) => {
+  async (error: AxiosError) => {
+    const originalRequest = error.config as AxiosRequestConfig & {
+      _retry: boolean;
+    };
+    if (
+      error.response?.status === 500 &&
+      ((error.response.data as object as { message: string })
+        .message as string) === "jwt expired"
+    ) {
+      if (isRefreshing && !originalRequest._retry) {
+        return new Promise((resolve, reject) =>
+          pendingQueue.push({ resolve, reject })
+        )
+          .then(() => axiosInstance(originalRequest))
+          .catch((error) => Promise.reject(error));
+      }
+      originalRequest._retry = true;
+      isRefreshing = true;
+      try {
+        await axiosInstance.post("/auth/refresh-token");
+        processQueue(null);
+        return axiosInstance(originalRequest);
+      } catch (error) {
+      
+        processQueue(error);
+        return Promise.reject(error);
+      } finally {
+        isRefreshing = false;
+      }
+    }
     return Promise.reject(error);
   }
 );
